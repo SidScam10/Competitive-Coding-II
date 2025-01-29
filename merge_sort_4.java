baadd62102664b8bf8f1253986686e4ad7b8c0d4
SidScam10 <siddhanth.monnappa@gmail.com>
2025-01-30 00:44:45 +0530

    code additions

---
+++ merge_sort_4.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class merge_sort_4 {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int merge_arr[]=new int[n];
+        for(int i=0;i<n;i++) merge_arr[i]=sc.nextInt();
+        Arrays.sort(merge_arr);
+        for(int i=0;i<n;i++) System.out.print(merge_arr[i]+" ");
+    }
+}
