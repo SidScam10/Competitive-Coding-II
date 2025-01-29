21237d9de994af306d13d6dea52911913eb02529
SidScam10 <siddhanth.monnappa@gmail.com>
2025-01-29 22:43:01 +0530

    code changes

--- loop_detect_scuffed.java
+++ loop_detect_scuffed.java
@@ -1,21 +1,20 @@
 import java.util.*;
-class odd_even_scuffed {
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int arr[]=new int[n];
-        int odd[]=new int[n];
-        int even[]=new int[n];
-        int odd_count=0,even_count=0;
-        for(int i=0;i<n;i++)
-        {
-            arr[i]=sc.nextInt();
-            if(arr[i]%2==0) even[even_count++]=arr[i];
-            else odd[odd_count++]=arr[i];
+
+public class loop_detect_scuffed {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
-        for(int i=n-1;i>=0;i--) System.out.print(arr[i]+" ");
-        System.out.println();
-        for(int i=even_count-1;i>=0;i--) System.out.print(even[i]+" ");
-        for(int i=odd_count-1;i>=0;i--) System.out.print(odd[i]+" ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + "->");
+        }
+        System.out.println("NULL");
+        int a = sc.nextInt(); //Position to take input to connect loop
+        if (a < 1 || a > n) System.out.println("Cycle not detected");
+        else System.out.println("Cycle detected");
+        sc.close();
     }
 }
